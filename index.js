23432046f092da2063b5806a11cd7afb581655c1
Surbhi1511-yadav <surbhi.yadav_cs19@gla.ac.in>
2022-02-22 23:44:42 +0530

    commits

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+//To generate a Random Number 
 let computerGuess;
 let userGuess = []; //Empty array
 
 const init = () => {
-    computerGuess = Math.random();
+    computerGuess = Math.floor(Math.random() * 100); //We are using *100 to get a number b/w 1-100 and math floor to remove the decimal value after *100
+    console.log(computerGuess);
 };
+
+// init();  We will call this function onload so go to index file 
